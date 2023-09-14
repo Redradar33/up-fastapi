248bf5d033ee5a9aa2826b2353852ea693468235
Redradar33 <141447266+Redradar33@users.noreply.github.com>
2023-09-14 12:01:27 -0700

    forRender

--- main.py
+++ main.py
@@ -33,4 +33,7 @@ async def get_instagram_url(request: Request, url: str = Form(...)):
         raise HTTPException(status_code=500, detail=f"Error: {e}")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+#uvicorn.run(app, host="127.0.0.1", port=8000)
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=10000)
