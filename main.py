af224e038ab05a13df6302006e978f46c2caa42f
Redradar33 <141447266+Redradar33@users.noreply.github.com>
2023-09-14 12:32:57 -0700

    u2

--- main.py
+++ main.py
@@ -34,6 +34,6 @@ async def get_instagram_url(request: Request, url: str = Form(...)):
 
 if __name__ == "__main__":
 #uvicorn.run(app, host="127.0.0.1", port=8000)
-    import uvicorn
+    #import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=10000)
+    #uvicorn.run(app, host="0.0.0.0", port=10000)
